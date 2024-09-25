473c75b1d047b2eae80d1ad0f52bc3531ba91452
Jakub Žák <zakj23@spstrutnov.cz>
2024-09-25 13:29:58 +0200

    kamen nuzky papir.......TEĎ!!

---
+++ projekty/projekty/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace projekty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] choices = { "kámen", "nůžky", "papír" };
+            Random random = new Random();
+
+            Console.WriteLine("Zvolte: 0 - kámen, 1 - nůžky, 2 - papír");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            if (userChoice < 0 || userChoice > 2)
+            {
+                Console.WriteLine("Neplatná volba!");
+                return;
+            }
+
+            int computerChoice = random.Next(0, 3);
+            Console.WriteLine($"Počítač zvolil: {choices[computerChoice]}");
+
+            // Vyhodnocení výsledku
+            if (userChoice == computerChoice)
+            {
+                Console.WriteLine("Remíza!");
+            }
+            else if ((userChoice == 0 && computerChoice == 1) ||
+                     (userChoice == 1 && computerChoice == 2) ||
+                     (userChoice == 2 && computerChoice == 0))
+            {
+                Console.WriteLine("Vyhráli jste!");
+            }
+            else
+            {
+                Console.WriteLine("Prohráli jste!");
+            }
+        }
+
+    }
+}
